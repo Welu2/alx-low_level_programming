89b81e73ea72def6107b616ee94dfdf3d43b434a
Welu2 <welelabekele5@gmail.com>
2023-02-19 12:56:20 -0800

    advanced

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include <unistd.h>
+
+
+
 /**
- * main - Entry point
- * Return 1
- */
+ * main - prints exactly and that place of art is useful
+ *
+ * Return: ALways 0
+*/
+
+
 int main(void)
+
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
