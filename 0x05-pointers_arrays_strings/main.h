6ff1e2a831e2c5d97b8c56c9b81d23d149b38fa2
Welu2 <welelabekele5@gmail.com>
2023-02-27 10:34:39 -0800

    reset

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+
+
 void reset_to_98(int *n);
 
 void swap_int(int *a, int *b);
@@ -21,3 +28,5 @@ void print_array(int *a, int n);
 int _putchar(char c);
 
 int _atoi(char *s);
+
+#endif
