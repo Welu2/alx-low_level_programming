0484938b3ebe28e80341767537db176dad7630bf
Welu2 <welelabekele5@gmail.com>
2023-03-03 15:28:22 -0800

    567

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,3 +9,9 @@ char *_strncpy(char *dest, char *src, int n);
 char *_strncat(char *dest, char *src, int n);
 
 char *_strcat(char *dest, char *src);
+
+char *cap_string(char *);
+
+char *leet(char *);
+
+char *string_toupper(char *);
